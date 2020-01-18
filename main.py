551f73094a9d86e24e36df03def3dec90d923293
Axel David Espinosa Meneses <axeldavid45@gmail.com>
2020-01-18 12:38:19 -0600

    Agregamos funciones a nuestro proyecto

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 
 clients = 'roberto, mariajose,'
 
-if __name__ == '__main__':
-    clients += 'axel'
+def list_clients():
     print(clients)
+
+
+def create_client(client_name):
+    global clients
+
+    clients += client_name
+    _add_comma()
+
+def _add_comma():
+    global clients
+
+    clients += ','
+
+
+if __name__ == '__main__':
+    list_clients()
+    create_client('axel')
+    list_clients()
 
