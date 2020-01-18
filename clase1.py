f0279593daa1c4da0a67cec7283f45aa90c72684
Axel David Espinosa Meneses <axeldavid45@gmail.com>
2020-01-18 12:30:59 -0600

    Comenzamos el proyecto de CRUD

---
+++ clase1.py
@@ -0,0 +1,15 @@
+import turtle
+
+window = turtle.Screen()
+
+tortuga = turtle.Turtle()
+
+tortuga.forward(100)
+tortuga.rigth(90)
+
+tortuga.forward(100)
+tortuga.rigth(90)
+
+
+window.mainloop()
+
---
+++ main.py
@@ -0,0 +1,7 @@
+
+clients = 'roberto, mariajose,'
+
+if __name__ == '__main__':
+    clients += 'axel'
+    print(clients)
+
