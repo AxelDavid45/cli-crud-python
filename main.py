8b8808413f5e859824c4150bf47af97b69f129d8
Axel David Espinosa Meneses <axeldavid45@gmail.com>
2020-01-29 21:20:46 -0600

    Agregamos un condicional para que el usuario elija la accion que desee realizar

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ def list_clients():
 
 def create_client(client_name):
     global clients
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client is already stored')
 
-    clients += client_name
-    _add_comma()
 
 def _add_comma():
     global clients
@@ -17,8 +20,26 @@ def _add_comma():
     clients += ','
 
 
+def _print_welcome():
+    print('Welcome to PLATZIVENTAS')
+    print('*' * 50)
+    print('What would you like to do?')
+    print('[C]reate client')
+    print('[D]elete client')
+
+
 if __name__ == '__main__':
-    list_clients()
-    create_client('axel')
-    list_clients()
+    _print_welcome()
+    command = input('Write a command: ')
+
+    if command == 'C':
+        client_name = input('Write client name: ')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    else:
+        print('Command does not exist')
+
+
 
