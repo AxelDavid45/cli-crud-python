94cf1d837ca60e83e7c1d8f6492cd7724cddbc4b
Axel David Espinosa Meneses <axeldavid45@gmail.com>
2020-01-30 23:43:31 -0600

    Agregamos la opcion de modificar un cliente

--- main.py
+++ main.py
@@ -14,6 +14,15 @@ def create_client(client_name):
         print('Client is already stored')
 
 
+def update_client(client_name, new_client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', new_client_name + ',')
+    else:
+        print('Client did not found')
+
+
+
 def _add_comma():
     global clients
 
@@ -26,18 +35,32 @@ def _print_welcome():
     print('What would you like to do?')
     print('[C]reate client')
     print('[D]elete client')
+    print('[U]pdate client')
+
+
+def _get_client_name():
+    return input('What is the client name?: ')
+
 
 
 if __name__ == '__main__':
     _print_welcome()
     command = input('Write a command: ')
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('Write client name: ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
         pass
+    elif command == 'U':
+        list_clients()
+        client_name = _get_client_name()
+        new_client_name = input('Write the new client name: ')
+        update_client(client_name, new_client_name)
+        list_clients()
+
     else:
         print('Command does not exist')
 
